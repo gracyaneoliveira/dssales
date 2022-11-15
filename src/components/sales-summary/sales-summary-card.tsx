2bd367634394231531d557a131351b51ea51b1a8
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-11-14 21:30:52 -0300

    Add SalesSummary component

--- src/components/sales-summary/sales-summary-card.tsx
+++ src/components/sales-summary/sales-summary-card.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './styles.css';
 
-function SalesSummaryCard() {
-  return <div className="sales-summary-card base-card"></div>;
+type Props = {
+  value: number;
+  label: string;
+  icon: React.ReactNode;
+};
+
+function SalesSummaryCard({ value, label, icon }: Props) {
+  return (
+    <div className="sales-summary-card base-card">
+      {icon}
+      <h3 className="sales-summary-card-value">{value}</h3>
+      <span className="sales-summary-card-label">{label}</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;
